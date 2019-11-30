fc467fcc45e539a314a15af41e31a476d3024e91
Leonardo Epifânio <leonardo.epifanio@tecnico.ulisboa.pt>
2019-11-30 12:46:32 +0000

    Add activity_time parser

--- project1/src/analysis.py
+++ project1/src/analysis.py
@@ -4,8 +4,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 from statistics import mean
+import datetime
+import math
 
 BINS = 1
+
+class Activity:
+    def __init__(self, userA, userB, timestamp, interaction):
+        self.userA = userA
+        self.userB = userB
+        self.timestamp = timestamp
+        self.interaction = interaction
+        self.activated = 0
+    
+    def __str__(self):
+        return "Activity: {0} {1} {2} {3}".format(self.userA, self.userB, self.timestamp, self.interaction)
+
+
+
+# Parses higgs-activity_time dataset
+def parse_activity_time(dataset):
+    activity_list = []
+    with open(dataset) as fin:
+            for line in fin:
+                attrs = line.split()
+                activity_list.append(Activity(int(attrs[0]), int(attrs[1]), int(attrs[2]), attrs[3]))
+    return activity_list
+
+
+
+# Builds activity_list
+def build_activated_users(activity_list):
+    activated_users = {}
+    for activity in activity_list:
+        if activity.userA not in activated_users:
+            activated_users[activity.userA] = 1
+            activity.activated = 1
+    activity_list = [activity for activity in activity_list if activity.activated == 1]
+    return activity_list
+
+
+
+def plot_infection_function(x, starting_date, initial_fraction, activation_rate):
+
+    plt.plot(x, 1 - (1 - initial_fraction) * math.exp(-activation_rate*(x - starting_date)), "-")
+
+
+# Plots activated users graphic
+def plot_activated_users(activated_users):
+    plt.figure()
+
+    xs = [(activated_user.timestamp - activated_users[0].timestamp)/(60*60*24) for activated_user in activated_users]
+    ys = range(1,len(activated_users)+1)
+    ys_frac = [float(i)/(len(activated_users)+1) for i in ys]
+
+    plt.plot(xs, ys_frac, marker=".", linestyle="", markersize=2)
+
+    #plot_infection_function(xs, 3.23541, 0.15685, 519.14)
+
+    plt.yscale("log")
+    plt.xlabel("Days from 1st July 2012")
+    plt.ylabel("Fraction of activated users")
+    plt.show()
 
 
 
@@ -218,21 +278,27 @@ if __name__ == '__main__':
     parser.add_argument("dataset")
     args = parser.parse_args()
 
-    # Read graph
-    graph = Graph.Read(args.dataset, "edgelist")
-    n_nodes = graph.vcount()
-    n_edges = graph.ecount()
-    
-    # Calculate degree distributions
-    calc_out_degree(graph)
-    calc_in_degree(graph)
-    calc_total_degree(graph)
+    if args.dataset == "input/higgs-social_network.edgelist":
+        # Read graph
+        graph = Graph.Read(args.dataset, "edgelist")
+        n_nodes = graph.vcount()
+        n_edges = graph.ecount()
+        
+        # Calculate degree distributions
+        calc_out_degree(graph)
+        calc_in_degree(graph)
+        calc_total_degree(graph)
 
-    # Calculate graph assortativity degree
-    calc_assort_degree(graph)
-    
-    # Calculate clustering properties
-    calc_clustering(graph, n_nodes, n_edges)
-    
-    # Calculate shortest path measures
-    calc_short_path(graph)
+        # Calculate graph assortativity degree
+        calc_assort_degree(graph)
+        
+        # Calculate clustering properties
+        calc_clustering(graph, n_nodes, n_edges)
+        
+        # Calculate shortest path measures
+        calc_short_path(graph)
+
+    elif args.dataset == "input/higgs-activity_time.txt":
+        activity_list = parse_activity_time(args.dataset)
+        activity_list = build_activated_users(activity_list)
+        plot_activated_users(activity_list)
