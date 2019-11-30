bfb0432633a022522fc76ca8bfff6faed400e56d
Pedro Daniel <pedro.daniel.l@tecnico.ulisboa.pt>
2019-11-30 21:32:24 +0000

    Split files and coreness plot

---
+++ project2/src/main.py
@@ -0,0 +1,33 @@
+from social_network import *
+from activity import *
+from igraph import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset")
+    args = parser.parse_args()
+
+    if args.dataset == "input/higgs-social_network.edgelist":
+        # Read graph
+        graph = Graph.Read(args.dataset, "edgelist")
+        n_nodes = graph.vcount()
+        n_edges = graph.ecount()
+        
+        # Calculate degree distributions
+        calc_out_degree(graph)
+        calc_in_degree(graph)
+        calc_total_degree(graph)
+
+        # Calculate graph assortativity degree
+        calc_assort_degree(graph)
+        
+        # Calculate clustering properties
+        calc_clustering(graph, n_nodes, n_edges)
+        
+        # Calculate shortest path measures
+        calc_short_path(graph)
+
+    elif args.dataset == "input/higgs-activity_time.txt":
+        activity_list = parse_activity_time(args.dataset)
+        activity_list = build_activated_users(activity_list)
+        plot_activated_users(activity_list)
